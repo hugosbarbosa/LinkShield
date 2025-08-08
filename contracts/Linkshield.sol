5f4999f29f0d1139cb136dba9415bfc910c6c6c2
hugosbarbosa <hugo.barbosa2005@hotmail.com>
2025-08-08 11:01:21 -0300

    a

--- contracts/Linkshield.sol
+++ contracts/Linkshield.sol
@@ -7,16 +7,21 @@ contract LinkShield {
         string url;
         address owner;
         uint256 fee;
+        
 
 
     }   
+
+    uint256 public commission = 1;
     mapping(string => Link) private links;
     mapping (string => mapping (address => bool) )public hasAccess;
+
 
     function addLink (string calldata url, string calldata linkId, uint256 fee) public {
 
         Link memory link = links[linkId];
         require( link.owner == address (0) || link.owner == msg.sender, "This linkId already has an owner!!");
+        require(fee == 0 || fee > commission, "Fee too low!!");
 
         link.url = url;
         link.owner = msg.sender;
@@ -24,6 +29,18 @@ contract LinkShield {
 
         links[linkId] = link;
         hasAccess[linkId][msg.sender] = true;
+    }
+    
+    function payLink (string calldata linkId) public payable {
+        Link memory link = links[linkId];
+        
+        require(link.owner != address (0) , "Link not found!!");
+        require(hasAccess[linkId][msg.sender] == false, "You already have access to this link!!");
+        require(msg.value >= link.fee, "Insufficient payment!!" );
+        
+        hasAccess[linkId][msg.sender] = true;
+        payable(link.owner).transfer(msg.value - commission);
+
     }
 
     function getLink (string calldata linkId) public view returns (Link memory) {
